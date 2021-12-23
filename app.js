3a384064d85d49607a35a01dc370c8553aaed024
kristiiii <94471403+kristiiii@users.noreply.github.com>
2021-12-23 16:26:26 +0400

    added index.html and app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const myString = 'My String value';
+const myNumber = 30;
+const myBoolean = true;
+const myBoolean1 = false;
+const myNull = null;
+const myUndefined = undefined;
+
+const myName = 'kristi';
+const myAge = '25';
+const myHobby = 'playing mobile games'
+
+console.log('My name is: §{myName} and my age is: §{myAge} and my hobby is: §{myHobby}');
